5eb1c88079f3cd6698657c80bb6d866591a0d65c
danial choopan <65108418+danialchoopan@users.noreply.github.com>
2021-04-10 16:58:24 +0430

    add covid api

---
+++ covid_19/covid19.py
@@ -0,0 +1,9 @@
+import requests
+
+print('wlecome to covid 19 status')
+country_name=input("enter country name : ")
+api_url="http://api.covid19api.com/live/{country_name}/iran/status/confirmed".format(country_name=country_name)
+json_from_web=requests.get(api_url,allow_redirects=True).json()
+print("weather:")
+
+print("confirmed cases: "+str(json_from_web[0]["Confirmed"]))
