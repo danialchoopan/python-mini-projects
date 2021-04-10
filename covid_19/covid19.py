76f45765ad180be73a856b14719aeccf7c5e0d2a
danial choopan <65108418+danialchoopan@users.noreply.github.com>
2021-04-10 23:35:52 +0430

    Update covid19.py

--- covid_19/covid19.py
+++ covid_19/covid19.py
@@ -4,10 +4,10 @@ print('wlecome to covid 19 status')
 
 country_name=input("enter country name : ")
 
-//api url
+#api url
 api_url="https://api.covid19api.com/live/country/{country_name}/status/confirmed".format(country_name=country_name)
 
-//send request to the api
+#send request to the api
 json_from_web=requests.get(api_url,allow_redirects=True).json()
 
 print("confirmed cases : "+str(json_from_web[0]["Confirmed"]))
